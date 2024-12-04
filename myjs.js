0e3e852d24ed543fc84fe73824a437352dd04bc2
daffasco <m.daffa07112008@gmail.com>
2024-12-04 11:24:47 +0700

    Fix.

---
+++ myjs.js
@@ -0,0 +1,10 @@
+// Toggle class active
+
+const ulnavbar = document.querySelector
+('.ul-navbar');
+
+// ketika menu di click
+document.querySelector('#menu').
+onclick = () => {
+    ulnavbar.classList.toggle('active');
+};
